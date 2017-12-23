67ab68dacfe369938b1c26c55f83b131b217a9ed
srinjoym <srinjoy.majumdar@datos.io>
2017-12-23 15:01:32 -0800

    Working optimizer with master always in view

--- src/bundle_generator.py
+++ src/bundle_generator.py
@@ -34,63 +34,34 @@ class BundleGenerator:
         l.minimize = True
         l.maxEvaluations = 2000
         print l.learn()
-        
-        #TESTING
-        print "-------TESTING-------"
-        print sorted(self.marker_locations.keys())
-        x0 = np.zeros(7)
-        l = CMAES(self.objF_master, x0)
-        l.minimize = True
-        l.maxEvaluations = 2000
-        print l.learn()
 
         print "keys"
         for marker_id in sorted(self.marker_locations.keys()):
-            if marker_id == 9:
-                for data in self.marker_locations[marker_id]:
-                    print data
+            print "Marker_id {0}".format(marker_id)
+            for data in self.marker_locations[marker_id]:
+                print data
 
-        print "angles"
-        for marker_id in sorted(self.angles.keys()):
-            if marker_id == 9:
-                for data in self.angles[marker_id]:
-                    print data
+        # print "angles"
+        # for marker_id in sorted(self.angles.keys()):
+        #     if marker_id == 9:
+        #         for data in self.angles[marker_id]:
+        #             print data
 
-
-    def objF_master(self, x):
-        sum = 0
-        for marker_id in sorted(self.marker_locations.keys()):
-            if marker_id == 9:
-                for pose in self.marker_locations[marker_id]:
-                    sum += np.sum(np.square(np.subtract(x[0:4],pose[0:4])))
-                    x_quat = Quaternion(x[6],x[3],x[4],x[5])
-                    pose_quat = Quaternion(pose[6],pose[3],pose[4],pose[5])
-                    dist = Quaternion.distance(x_quat,pose_quat)
-                    print "distance is {0}".format(dist*dist)
-                    # print "sum is {0}".format(sum)
-                    sum += dist*dist
-
-        return sum
 
     def objF(self, x):
-        # print "x arr {0}".format(x.shape)
         x.shape = (len(self.marker_locations),7)
-        # print "x arr {0}".format(x)
         sum = 0
         i = 0
+
         for marker_id in sorted(self.marker_locations.keys()):
-            for data in self.marker_locations[marker_id]:
-                # print "marker_id:{0}".format(marker_id)
-                # print "data: {0}".format(data)
-                # print "x0: {0}".format(x[i])
-                # print np.sum(np.square(np.subtract(x[i],data)))
-                trans = data[0:3]
-                rot = data[3:]
-                # print "trans: {0}, rot: {1}".format(trans,rot)
-                sum += np.sum(np.square(np.subtract(x[i],data)))
-            # sum +=
-            # print marker_id, len(trans)
-        # print sum
+            for pose in self.marker_locations[marker_id]:
+                sum += np.sum(np.square(np.subtract(x[i][0:4],pose[0:4])))
+                x_quat = Quaternion(x[i][6],x[i][3],x[i][4],x[i][5])
+                pose_quat = Quaternion(pose[6],pose[3],pose[4],pose[5])
+                dist = Quaternion.distance(x_quat,pose_quat)
+                print "distance is {0}".format(dist*dist)
+                sum += dist*dist
+            i+=1
         return sum
 
     def stop_record(self):
@@ -111,7 +82,6 @@ class BundleGenerator:
             try:
                 trans = self.tfBuffer.lookup_transform(
                     master_name, marker_name, rospy.Time())
-                print "transform from {0} to {1} is {2}".format(master_name,marker_name,trans)
                 if marker.id in self.marker_locations:
                     self.marker_locations[marker.id].append(self.tf_to_array(trans.transform,marker.id))
                 else:
@@ -135,27 +105,15 @@ class BundleGenerator:
         else:
             self.angles[id_] = [euler_pose[3:6]]
         euler_pose = simplify_pose(euler_pose)
-        quat_pose = []
         quat = tf.transformations.quaternion_from_euler(euler_pose[3], euler_pose[4], euler_pose[5])
-        # print "simplified {0}".format(np.concatenate((np.array(euler_pose[0:3]), quat)))
-        print "simplified {0}".format(np.concatenate((np.array(euler_pose[0:3]), np.array([pose.rotation.x, pose.rotation.y, pose.rotation.z, pose.rotation.w]))))
         return np.concatenate((np.array(euler_pose[0:3]), quat))
 
 def simplify_pose(pose):
-    # print "non simplified pose {0}".format(pose)
     for i in range(0,3):
         if(pose[i]<0):
             pose[i] = -pose[i]
             pose[i+3] = pose[i+3] + 180
-    for i in range(3,6):
-        while(pose[i]>180):
-            pose[i] -= 360
-        while(pose[i]<-180):
-            pose[i] += 360
-    # print "simplified pose {0}".format(pose)
     return pose
-
-
 
 def main():
     gen = BundleGenerator()
