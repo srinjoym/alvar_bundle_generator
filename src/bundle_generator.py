bf2af6486fff9a7cf63e0698cee87e8247e0c247
srinjoym <srinjoy.majumdar@datos.io>
2017-12-31 17:15:06 -0800

    code for no master, but optimization on raw position of tags

--- src/bundle_generator.py
+++ src/bundle_generator.py
@@ -14,38 +14,22 @@ import xml.etree.cElementTree as ET
 
 class BundleGenerator:
     def __init__(self):
-        print "in init"
+        print "Initializing BundleGenerator"
         self.master_id = None
-        self.marker_locations = {}
-        self.bundle_locations = {}
+        self.master_pose = None
+        self.marker_buffer = {}
+        self.optimized_marker_poses = {}
         self.point_locations = {}
-        self.angles = {}
-        # rospy.Subscriber(image_topic,Image, self.process_img, queue_size=1)
+
         self.ar_sub = rospy.Subscriber("ar_pose_marker", AlvarMarkers, self.callback)
         self.tfBuffer = tf2_ros.Buffer()
         listener = tf2_ros.TransformListener(self.tfBuffer)
         self.optimize_id = -1
         self.marker_size = 4.75/100
-        # rospy.sleep(5)
         np.set_printoptions(suppress=True)
-        print "finished init"
 
-    def learn_pose(self):
-        for marker_id in sorted(self.marker_locations.keys()):
-            x0 = np.zeros(7)
-            self.optimize_id = marker_id
-
-            l = CMAES(self.objF, x0)
-            l.minimize = True
-            l.maxEvaluations = 1000
-
-            pose = l.learn()
-            print pose
-
-            self.bundle_locations[marker_id] = pose[0]
-
-        self.generate_points()
-        self.create_bundle_file()
+        rospy.sleep(3)
+        print "Finished Init BundleGenerator"
 
     def add_marker_to_file(self,root, tag_id):
         marker = ET.SubElement(root, "marker",index=str(tag_id),status=str(1))
@@ -79,41 +63,51 @@ class BundleGenerator:
         return p_bl,p_br,p_tr,p_tl
 
     def generate_points(self):
-        for marker_id in sorted(self.bundle_locations.keys()):
-            # if(marker_id==self.master_id):
-            pose = self.bundle_locations[marker_id]
-            # print "quaternion {0}".format(pose)
-
+        for marker_id in sorted(self.optimized_marker_poses.keys()):
+            pose = self.optimized_marker_poses[marker_id]
             rot = tf.transformations.euler_from_quaternion(pose[3:7])
             matrix = tf.transformations.compose_matrix(translate=pose[0:3], angles=rot)
-            # print "transformation matrix \n {0}".format(matrix.shape)
-
             points = self.points_from_pose(pose)
-
             res = []
             for point in points:
-                # print "origin_point \n {0}".format(point)
                 dot = np.array([point[0],point[1],point[2],1]).T.dot(matrix)
                 res.append(dot[:3])
-
             self.point_locations[marker_id] = res
-
-        print self.point_locations
 
     def objF(self, x):
         sum = 0
-        # print "begin optimize"
-
-        for pose in self.marker_locations[self.optimize_id]:
+        for pose in self.marker_buffer[self.optimize_id]:
             sum += np.sum(np.square(np.subtract(x[0:4],pose[0:4])))
             x_quat = Quaternion(x[6],x[3],x[4],x[5])
             pose_quat = Quaternion(pose[6],pose[3],pose[4],pose[5])
             dist = Quaternion.absolute_distance(x_quat,pose_quat)/3.14
-            sum += dist*dist
-
-        # print "marker {0} sum {1}".format(self.optimize_id, sum)
-        # print "end optimize"
+            sum += dist*dist 
         return sum
+
+    def optimize_pose(self, marker_id, save_transform=True):
+        x0 = np.zeros(7)
+        self.optimize_id = marker_id
+        l = CMAES(self.objF, x0)
+        l.minimize = True
+        l.maxEvaluations = 1000
+        pose = l.learn()
+        print marker_id, pose #DEBUG
+        #TODO transform relative to master pose
+        if save_transform:
+            self.optimized_marker_poses[marker_id] = compute_transform(self.master_pose, pose[0])
+        return pose[0]
+
+    def learn_pose(self):
+        self.master_id = find_max_count(self.marker_buffer)
+        self.master_pose = self.optimize_pose(self.master_id, save_transform=False)
+        print "master tag is {0}".format(self.master_id)
+        print "computer transform test {0}".format(compute_transform(self.master_pose,self.master_pose))
+        for marker_id in sorted(self.marker_buffer.keys()):
+            self.optimize_pose(marker_id)
+        print self.marker_buffer
+        print self.optimized_marker_poses #DEBUG
+        self.generate_points()
+        self.create_bundle_file()
 
     def stop_record(self):
         self.ar_sub.unregister()
@@ -122,52 +116,47 @@ class BundleGenerator:
         raw_markers = msg.markers
         if len(raw_markers) < 1:
             return
-
-        if not self.master_id:
-            self.master_id = raw_markers[0].id
-            #Debugs
-            # self.master_id = 0
-
-        master_name = 'ar_marker_{0}'.format(self.master_id)
-
+        print "callback" #DEBUG
         for marker in raw_markers:
-            marker_name = "ar_marker_{0}".format(marker.id)
-            try:
-                time = rospy.Time()
-                trans = self.tfBuffer.lookup_transform(
-                    master_name, marker_name, time)
-                print "trans from {0} to {1} is {2}".format(master_name,marker_name,trans)
-                if marker.id in self.marker_locations:
-                    self.marker_locations[marker.id].append(self.tf_to_array(trans.transform,marker.id))
-                else:
-                    self.marker_locations[marker.id] = [self.tf_to_array(trans.transform,marker.id)]
-            except Exception, e:
-                print "No Transform {0}".format(e)
-                return
+            if marker.id in self.marker_buffer:
+                self.marker_buffer[marker.id].append(standardize_pose(marker.pose.pose))
+            else:
+                self.marker_buffer[marker.id] = [standardize_pose(marker.pose.pose)]
 
-    def tf_to_array(self,pose, id_):
-        angle = tf.transformations.euler_from_quaternion([
-        pose.rotation.x, pose.rotation.y, pose.rotation.z, pose.rotation.w])
-        euler_pose = []
-        euler_pose.append(pose.translation.x)
-        euler_pose.append(pose.translation.y)
-        euler_pose.append(pose.translation.z)
-        euler_pose.append(degrees(angle[0]))
-        euler_pose.append(degrees(angle[1]))
-        euler_pose.append(degrees(angle[2]))
-        if id_ in self.angles:
-            self.angles[id_].append(euler_pose[3:6])
-        else:
-            self.angles[id_] = [euler_pose[3:6]]
-        euler_pose = simplify_pose(euler_pose)
-        quat = tf.transformations.quaternion_from_euler(radians(euler_pose[3]), radians(euler_pose[4]), radians(euler_pose[5]))
-        return np.concatenate((np.array(euler_pose[0:3]), quat))
+def compute_transform(p1, p2):
+    res = np.zeros(7)
+    res[0:3] = p2[0:3]-p1[0:3]
+    p1[6] = -p1[6]
+    q2, q1 = Quaternion(p2[6],*p2[3:6]), Quaternion(p1[6],*p1[3:6])
+    res[3:7] = (q2/q1).elements
+    res[3], res[6] = res[6], res[3]
+    return res
+
+def find_max_count(dict):
+    max_count, max_id = -1,-1
+    for key,value in dict.iteritems():
+        if(len(value)>max_count):
+            max_id = key
+            max_count = len(value)
+    return max_id
+
+def standardize_pose(pose):
+    trans = pose.position
+    orient = pose.orientation
+    angle = tf.transformations.euler_from_quaternion([
+    orient.x, orient.y, orient.z, orient.w])
+    euler_pose = [trans.x,trans.y,trans.z, degrees(angle[0]), degrees(angle[1]),degrees(angle[2])]
+    euler_pose = simplify_pose(euler_pose)
+    quat = tf.transformations.quaternion_from_euler(radians(euler_pose[3]), radians(euler_pose[4]), radians(euler_pose[5]))
+    return np.concatenate((np.array(euler_pose[0:3]), np.array([orient.x,orient.y,orient.z,orient.w])))
 
 def simplify_pose(pose):
-    for i in range(0,3):
-        if(pose[i]<0):
-            pose[i] = -pose[i]
-            pose[i+3] = pose[i+3] + 180
+    # for i in range(0,3):
+    #     if(pose[i]<0):
+    #         pose[i] = -pose[i]
+    #         pose[i+3] = pose[i+3] + 180
+    for i in range(3,6):
+        while (pose[i]<0): pose[i]+=360
     return pose
 
 def main():
