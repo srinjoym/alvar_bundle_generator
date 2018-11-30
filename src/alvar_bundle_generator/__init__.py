6dd4c6ddbd3b2d6d0114c6cc5fe603920d32ae2b
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-11-30 07:24:42 -0600

    more code cleanup

--- src/alvar_bundle_generator/__init__.py
+++ src/alvar_bundle_generator/__init__.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
 from alvar_bundle_generator import main
 
+def print_usage():
+  print "usage: alvar_bundle_generator marker_size master_tag_id"
+  print " marker_size: length of one side of tag (in cm)"
+  print " master_tag_id: ID of tag that should be master in bundle file"
+
 if __name__ == '__main__':
-    rospy.init_node(
-        'alvar_bundle_generator', anonymous=True)
-    main()
+    if len(sys.argv) < 2:
+      print_usage()
+    else:
+      rospy.init_node(
+          'alvar_bundle_generator', anonymous=True)
+      main()
